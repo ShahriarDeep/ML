69f7a9c8b21591a681cc7581d7b2ab281c0c503c
ShahriarDeep <shahriarmursalindeep@gmail.com>
2024-10-24 23:56:25 +0600

    Add files via upload

---
+++ math_userinput.py
@@ -0,0 +1,33 @@
+# Find the smallest number from the given series
+x = min(20, 40, 60)
+print("The smallest number from the given series is:", x)
+
+# Find the largest number of the series
+y = max(20, 10, 25)
+print("The largest number from the given series is:", y)
+
+# For x power of y
+z = pow(x, y)
+print("The result is:", z)
+
+# Import the value of pi
+import math
+pi = math.pi
+print("The value of pi is:", pi)
+
+# Making integers from decimals
+
+# For the nearest high
+s = math.ceil(5.4)
+print(s)
+
+# For the nearest low
+r = math.floor(4.4)
+print(r)
+
+# Taking input from user
+username = input("Enter your Username: ")
+print("Username is: " + username)
+
+password = input("Enter your Password: ")
+print("Password is: " + password)
