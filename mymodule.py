25876b77d0181258f0a52438e5588e8e38190c7b
ShahriarDeep <shahriarmursalindeep@gmail.com>
2024-10-24 23:17:26 +0600

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,19 @@
+#Decalring module: mymodule.py
+
+Bangladesh = {
+    "Capital": "Dhaka",
+    "Birth": 1971,
+    "Continent": "Asia"
+}
+
+India = {
+    "Capital": "Delhi",
+    "Birth": 1947,
+    "Continent": "Asia"
+}
+
+Pakistan = {
+    "Capital": "Islamabad",
+    "Birth": 1947,
+    "Continent": "Asia"
+}
